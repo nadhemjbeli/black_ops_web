6479a7e6389a220c62459deff128a77e17bc85a9
Fritchou2 <mohamedaziz.ferchichi@esprit.tn>
2022-04-05 02:51:58 +0100

    Admin

---
+++ src/Controller/BackOfficeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BackOfficeController extends AbstractController
+{
+    /**
+     * @Route("/back/office", name="app_back_office")
+     */
+    public function index(): Response
+    {
+        return $this->render('back_office/index.html.twig', [
+            'controller_name' => 'BackOfficeController',
+        ]);
+    }
+}
